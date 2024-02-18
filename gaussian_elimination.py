b189db6b1db018a49853c3e30f62675d99c11b88
shulamitmorjossef <117088218+shulamitmorjossef@users.noreply.github.com>
2024-02-18 21:46:25 +0200

    yes!

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -1,6 +1,7 @@
 import numpy as np
 from colors import bcolors
 from matrix_utility import swap_row
+from condition_of_linear_equations import condition_number
 
 
 def gaussianElimination(mat):
@@ -72,17 +73,64 @@ def backward_substitution(mat):
         for j in range(i + 1, N):
             x[i] -= mat[i][j] * x[j]
 
+
+        if round(mat[i][i], 3) == 0 :
+            return "no solution "
+
+
         x[i] = (x[i] / mat[i][i])
 
     return x
 
 
 if __name__ == '__main__':
+    print("date:19/2/24\n"
+          "group members: \n"
+          "(1) name: Shulamit-mor-yossef. id: 206576977. \n"
+          "(2) name: Zohar-monsonego. id: 214067662. \n"
+          "(3) name: hodaya-shirazie. id: 213907785.\n"
+          "submitted by: name: Shulamit Mor Yossef, id: 206576977.\n"
+          "input: \n"
+          "output:\n ")
+    # A_b = [[2, 1.7, -2.5, 1],
+    #               [1.24, -2, -0.5, 1],
+    #               [3, 0.2, 1, 0]]
 
-    A_b = [[1, -1, 2, -1, -8],
-        [2, -2, 3, -3, -20],
-        [1, 1, 1, 0, -2],
-        [1, -1, 4, 3, 4]]
+    # A_b = [[1, -1, 2, -1, -8],
+    #     [2, -2, 3, -3, -20],
+    #     [1, 1, 1, 0, -2],
+    #     [1, -1, 4, 3, 4]]
+
+    A_b = [[0, 1, 2, -1],
+           [1, 0, 1, -3],
+           [2, 3, 0, 0]]
+
+    A_b_s = np.array([[0, 1, 2],
+                      [1, 0, 1],
+                      [2, 3, 0]])
+
+
+    # A_b = [[1, -1, 2, -1, 9],
+    #        [2, -2, 3, -3, 90],
+    #        [1, 1, 1, 0, 0],
+    #        [1, -1, 4, 3, 7]]
+    #
+    #
+    # A_b_s =np.array([[1, -1, 2, -1],
+    #        [2, -2, 3, -3],
+    #        [1, 1, 1, 0],
+    #        [1, -1, 4, 3]])
+    # A_b = [[1, -1, 2, -1, -8],
+    #     [2, -2, 3, -3, -20],
+    #     [1, 1, 1, 0, -2],
+    #     [1, -1, 4, 3, 4]]
+    # if 9:
+    #     print("hi")
+    cond_matrix = condition_number(A_b_s)
+    if int(cond_matrix) > 2:
+        print(bcolors.HEADER, f"pay attention, cond A is {cond_matrix} , solution may not be accurate")
+    print(" cond is: " + str(cond_matrix) , bcolors.ENDC )
+
 
     result = gaussianElimination(A_b)
     if isinstance(result, str):
